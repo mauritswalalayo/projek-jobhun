50fb0548a2f8072ce311d4d6a0588d5d246759c3
Zunedi <zunedisinambela@gmail.com>
2019-06-24 11:18:27 +0700

    Upload

---
+++ resources/views/user/partial/js.blade.php
@@ -0,0 +1,8 @@
+<script src="{{ asset ('user/js/jquery.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset ('user/js/modernizr.js')}}" type="text/javascript"></script>
+<script src="{{ asset ('user/js/script.js')}}" type="text/javascript"></script>
+<script src="{{ asset ('user/js/bootstrap.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset ('user/js/wow.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset ('user/js/slick.min.js')}}" type="text/javascript"></script>
+<script src="{{ asset ('user/js/parallax.js')}}" type="text/javascript"></script>
+<script src="{{ asset ('user/js/select-chosen.js')}}" type="text/javascript"></script>
