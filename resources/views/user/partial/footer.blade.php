6b9f8edf6b4c173268b750c37976f19035b60538
Zunedi <zunedisinambela@gmail.com>
2019-06-24 18:39:10 +0700

    Upload

--- resources/views/user/partial/footer.blade.php
+++ resources/views/user/partial/footer.blade.php
@@ -6,19 +6,12 @@
                     <div class="widget">
                         <div class="about_widget">
                             <div class="logo">
-                                <a href="#" title=""><img src="http://placehold.it/178x40" alt="" /></a>
+                                <a href="{{route('index')}}" title=""><img src="{{ asset('user/images/jobhun.png')}}" width="178" height="40" alt="" /></a>
                             </div>
                             <span>Semua hal yang dibutuhkan para pencari kerja maupun perusahaan yang sedang
                                 mencari pekerja ada di Jobhun. Pencari kerja dapat menemukan pekerjaan,
                                 sekaligus mengembangkan kemampuannya. Sedangkan perusahaan bisa mencari pekerja
                                 dengan cara yang cepat dan mudah.</span>
-                            <div class="social">
-                                <a href="#" title=""><i class="fa fa-facebook"></i></a>
-                                <a href="#" title=""><i class="fa fa-twitter"></i></a>
-                                <a href="#" title=""><i class="fa fa-linkedin"></i></a>
-                                <a href="#" title=""><i class="fa fa-pinterest"></i></a>
-                                <a href="#" title=""><i class="fa fa-behance"></i></a>
-                            </div>
                         </div><!-- About Widget -->
                     </div>
                 </div>
@@ -45,7 +38,7 @@
                             <hr>
                             <span>Geco Coworking Space, Kompleks AJBS, Jl. Ratna 14, Ngagel, Wonokromo,
                                 Surabaya, Jawa Timur, Indonesia 60246.</span>
-                            <span>E-mail: info@jobhun.id.</span>
+                            <span>E-mail: info@jobhun.id</span>
                             <span>Telp. & WA: 08113235533.</span>
                         </div><!-- About Widget -->
                     </div>
@@ -53,23 +46,26 @@
                 <div class="col-md-3 column">
                     <div class="widget">
                         <h3 class="footer-title">Media Sosial Jobhun</h3>
-                        <div class="link_widgets">
-                            <div class="row">
-                                <div class="col-lg-12">
-
-                                </div>
+                        <div class="about_widget">
+                            <div class="social">
+                                <a href="https://www.youtube.com/channel/UCCzKrMp711xzX8nI8zempKw?view_as=subscriber" title=""><i class="fa fa-youtube"></i></a>
+                                <a href="https://www.instagram.com/jobhun/" title=""><i class="fa fa-instagram"></i></a>
+                                <a href="https://www.linkedin.com/company/jobhun-surabaya/" title=""><i class="fa fa-linkedin"></i></a>
                             </div>
+                        </div><!-- About Widget -->
+                        <div class="about_widget">
+                            <h3 class="footer-title">Dapatkan update terbaru dari Jobhun!</h3>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 column">
+                {{-- <div class="col-lg-3 column">
                     <div class="widget">
                         <div class="download_widget">
                             <a href="#" title=""><img src="http://placehold.it/230x65" alt="" /></a>
                             <a href="#" title=""><img src="http://placehold.it/230x65" alt="" /></a>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
