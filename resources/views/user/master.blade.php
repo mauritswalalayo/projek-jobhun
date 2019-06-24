da664f0cd571ce8c2620079f9b48d4e900f0f0a7
Zunedi <zunedisinambela@gmail.com>
2019-06-24 14:22:47 +0700

    Upload

--- resources/views/user/master.blade.php
+++ resources/views/user/master.blade.php
@@ -30,8 +30,8 @@
 				<div class="btn-extars">
 					<a href="#" title="" class="post-job-btn"><i class="la la-plus"></i>Post Jobs</a>
 					<ul class="account-btns">
-						<li class="signup-popup"><a title=""><i class="la la-key"></i> Daftar</a></li>
-						<li class="signin-popup"><a title=""><i class="la la-external-link-square"></i> Masuk</a></li>
+						<li class="signup-popup"><a title=""><i class="la la-key"></i> Sign Up</a></li>
+						<li class="signin-popup"><a title=""><i class="la la-external-link-square"></i> Login</a></li>
 					</ul>
 				</div><!-- Btn Extras -->
 				<form class="res-search">
@@ -85,8 +85,8 @@
 					<div class="btn-extars">
 						<a href="#" title="" class="post-job-btn"><i class="la la-plus"></i>Post Jobs</a>
 						<ul class="account-btns">
-							<li class="signup-popup"><a title=""><i class="la la-key"></i> Daftar</a></li>
-							<li class="signin-popup"><a title=""><i class="la la-external-link-square"></i> Masuk</a>
+							<li class="signup-popup"><a title=""><i class="la la-key"></i> Sign Up</a></li>
+							<li class="signin-popup"><a title=""><i class="la la-external-link-square"></i> Login</a>
 							</li>
 						</ul>
 					</div><!-- Btn Extras -->
