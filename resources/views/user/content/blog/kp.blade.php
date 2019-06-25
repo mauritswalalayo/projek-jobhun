6db11797661f45fd696a6596e14fb7a545e1b801
Zunedi <zunedisinambela@gmail.com>
2019-06-25 19:48:18 +0700

    Upload

--- resources/views/user/content/blog/kp.blade.php
+++ resources/views/user/content/blog/kp.blade.php
@@ -1,16 +1,24 @@
 @extends('user.master')
 
 @section('content')
-<section class="overlape">
-	<div class="block no-padding">
-		<div data-velocity="-.1"
-			style="background: url(http://placehold.it/1600x800) repeat scroll 50% 422.28px transparent;"
-			class="parallax scrolly-invisible no-parallax"></div><!-- PARALLAX BACKGROUND IMAGE -->
-		<div class="container fluid">
+
+<section>
+	<div class="block">
+		<div class="container">
 			<div class="row">
 				<div class="col-lg-12">
-					<div class="inner-header">
-						<h3>Blog</h3>
+					<div class="about-us">
+						<div class="row">
+							<div class="col-lg-12">
+							</div>
+							<div class="col-lg-7">
+							</div>
+							<div class="col-lg-12">
+								<br><br>
+
+								<h2 align="center">Kategori : Karierpedia</h2>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
