75e408803c27a9540413d4226835b7152f35927d
Zunedi <zunedisinambela@gmail.com>
2019-06-25 19:35:09 +0700

    Upload

--- resources/views/user/content/program/ji.blade.php
+++ resources/views/user/content/program/ji.blade.php
@@ -1,22 +1,43 @@
 @extends('user.master')
 
 @section('content')
-<section class="overlape">
-	<div class="block no-padding">
-		<div data-velocity="-.1"
-			style="background: url(http://placehold.it/1600x800) repeat scroll 50% 422.28px transparent;"
-			class="parallax scrolly-invisible no-parallax"></div><!-- PARALLAX BACKGROUND IMAGE -->
-		<div class="container fluid">
+
+<section>
+	<div class="block">
+		<div class="container">
 			<div class="row">
 				<div class="col-lg-12">
-					<div class="inner-header">
-						<h3>Blog</h3>
+					<div class="about-us">
+						<div class="row">
+							<div class="col-lg-12">
+							</div>
+							<div class="col-lg-7">
+							</div>
+							<div class="col-lg-12">
+								<br><br>
+
+								<h2 align="center">Kategori : Jobhun Internship</h2>
+								<p align="justify">
+									Sebuah program magang dari Jobhun yang ditujukan bagi para pelajar atau mahasiswa,
+									dengan rentang usia 15-22 tahun. Posisi magang yang dibuka biasanya menyesuaikan
+									kebutuhan proyek Jobhun, seperti content writer, social media specialist, graphic
+									designer, videographer, creative team, business development, marketing, hingga
+									technical. Dalam program ini, mereka akan mendapatkan pelatihan atau mentoring
+									beberapa topik (kepenulisan, desain, social media management, pembuatan acara, dll).
+									Mereka juga berkesempatan untuk membantu tim inti Jobhun mengerjakan beberapa proyek
+									dan pekerjaan rutin. Para pemagang juga berkesempatan untuk menjadi tim Jobhun
+									setelah program selesai, apabila memenuhi kriteria. Untuk mengetahui informasi
+									Jobhun Internship lebih detail, kamu bisa melihat update konten di bawah ini.
+								</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
+
 
 <section>
 	<div class="block">
